4ad0792285b633a750b06daae9af530c240567b0
Edward <sp576@cornell.edu>
2013-06-04 14:03:48 -0400

    prepare

--- homepage/settings.py
+++ homepage/settings.py
@@ -56,8 +56,8 @@ MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
-
+#MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_URL = 'http://limitless-falls-7440.herokuapp.com/media/'
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -73,7 +73,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	"C:/users/lonelycube/homepage/static/",
+	#"C:/users/lonelycube/homepage/static/",
+	"http://limitless-falls-7440.herokuapp.com/static/",
 )
 
 # List of finder classes that know how to find static files in
