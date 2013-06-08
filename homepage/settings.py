22b52d2455776fa7077b056702aa275563d1dc58
Edward <sp576@cornell.edu>
2013-06-07 22:19:58 -0400

    wtf

--- homepage/settings.py
+++ homepage/settings.py
@@ -19,19 +19,6 @@ DATABASES = {
 		}
 }
 
-if DEBUG:
-	DATABASES = {
-			'default': {
-					'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.                 # Or path to database file if using sqlite3.
-					# The following settings are not used with sqlite3:
-					'NAME': 'homepage',
-					'USER': 'lonelycube',
-					'PASSWORD': 'green1226',
-					'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-					'PORT': '',                      # Set to empty string for default.
-			}
-	}
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
@@ -179,10 +166,9 @@ LOGGING = {
         },
     }
 }
-if not DEBUG:
-	# Parse database configuration from $DATABASE_URL
-	import dj_database_url
-	DATABASES['default'] =  dj_database_url.config()
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-	# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
