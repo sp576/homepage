30fc761925600da74d48993aa668e24bc66df7d0
Sungho Park <sp576@cornell.edu>
2014-01-15 18:32:08 -0800

    Update settings.py

--- homepage/settings.py
+++ homepage/settings.py
@@ -12,8 +12,8 @@ DATABASES = {
 				'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.                 # Or path to database file if using sqlite3.
 				'NAME': 'homepage',
 				# The following settings are not used with sqlite3:
-				'USER': 'lonelycube',
-				'PASSWORD': 'green1226',
+				'USER': 'hello',
+				'PASSWORD': 'world',
 				'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
 				'PORT': '',                      # Set to empty string for default.
 		}
